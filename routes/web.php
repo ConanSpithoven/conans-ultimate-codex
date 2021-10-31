8f6087d2228197d5d53e2ac70e0c6f4cbcbc558a
Conan <conan.spithoven@gmail.com>
2021-10-31 15:27:41 +0100

    implemented login/logout/register into menu

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/', function()
     return View::make('pages.main');
 });
 
+Route::get('/dashboard', function(){
+    return View('pages.main');
+});
+
 Route::get('creatures/review', [CreatureController::class, 'review'])->name('creatures.review');
 Route::get('creatures/admin', [CreatureController::class, 'admin'])->name('creatures.admin');
 Route::resource('creatures', CreatureController::class);
